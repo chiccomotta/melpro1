85fd42f67b73eeba9a6252d46532e605e2ba879f
cristiano motta <cristiano.motta@hotmail.it>
2025-08-22 15:51:25 +0200

    fix renaming table

--- run_pipeline.bat
+++ run_pipeline.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Configuration
 set PGPASSWORD=postgres
-set TABLE_NAME=Customers
+set TABLE_NAME=customers_staging
 set COPY_TABLE_NAME=CustomersCopy
 
 REM Execute the SQL command to drop the table
@@ -20,21 +20,14 @@ if %ERRORLEVEL% NEQ 0 (
     exit /b 1
 )
 
-REM Change table schema
-echo Changing schema for table "%TABLE_NAME%"...
-psql -h localhost -U postgres -d targetdb -c "ALTER TABLE tap_mssql.\"%TABLE_NAME%\" SET SCHEMA public;"
-if %ERRORLEVEL% NEQ 0 (
-    echo Error while executing the SQL command to change the schema. Terminating the script.
-    exit /b 1
-)
 
-REM Copy table in a transaction
-echo Copying table "%TABLE_NAME%" to "%COPY_TABLE_NAME%"...
-psql -h localhost -U postgres -d targetdb -c "BEGIN; CREATE TABLE public.\"%COPY_TABLE_NAME%\" (LIKE public.\"%TABLE_NAME%\" INCLUDING ALL); INSERT INTO public.\"%COPY_TABLE_NAME%\" SELECT * FROM public.\"%TABLE_NAME%\"; COMMIT;"
-if %ERRORLEVEL% NEQ 0 (
-    echo Error while copying the table "%TABLE_NAME%" to "%COPY_TABLE_NAME%". Terminating the script.
-    exit /b 1
-)
+@REM REM Copy table in a transaction
+@REM echo Copying table "%TABLE_NAME%" to "%COPY_TABLE_NAME%"...
+@REM psql -h localhost -U postgres -d targetdb -c "BEGIN; DROP TABLE IF EXISTS public.\"%COPY_TABLE_NAME%\"; CREATE TABLE public.\"%COPY_TABLE_NAME%\" (LIKE public.\"%TABLE_NAME%\" INCLUDING ALL); INSERT INTO public.\"%COPY_TABLE_NAME%\" SELECT * FROM public.\"%TABLE_NAME%\"; COMMIT;"
+@REM if %ERRORLEVEL% NEQ 0 (
+@REM     echo Error while copying the table "%TABLE_NAME%" to "%COPY_TABLE_NAME%". Terminating the script.
+@REM     exit /b 1
+@REM )
 
 REM Final success message
 echo Pipeline completed successfully!
