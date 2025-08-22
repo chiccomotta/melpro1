57a8a7e79c095da2c6c34750302589ae3a1879c5
cristiano motta <cristiano.motta@hotmail.it>
2025-08-22 17:44:59 +0200

    Minor changes

--- run_pipeline.bat
+++ run_pipeline.bat
@@ -21,13 +21,14 @@ if %ERRORLEVEL% NEQ 0 (
 )
 
 
-@REM REM Copy table in a transaction
-@REM echo Copying table "%TABLE_NAME%" to "%COPY_TABLE_NAME%"...
-@REM psql -h localhost -U postgres -d targetdb -c "BEGIN; DROP TABLE IF EXISTS public.\"%COPY_TABLE_NAME%\"; CREATE TABLE public.\"%COPY_TABLE_NAME%\" (LIKE public.\"%TABLE_NAME%\" INCLUDING ALL); INSERT INTO public.\"%COPY_TABLE_NAME%\" SELECT * FROM public.\"%TABLE_NAME%\"; COMMIT;"
-@REM if %ERRORLEVEL% NEQ 0 (
-@REM     echo Error while copying the table "%TABLE_NAME%" to "%COPY_TABLE_NAME%". Terminating the script.
-@REM     exit /b 1
-@REM )
+REM Copy table in a transaction
+echo Copying table "%TABLE_NAME%" to "%COPY_TABLE_NAME%"...
+psql -h localhost -U postgres -d targetdb -c "BEGIN; DROP TABLE IF EXISTS public.\"%COPY_TABLE_NAME%\"; CREATE TABLE public.\"%COPY_TABLE_NAME%\" (LIKE public.\"%TABLE_NAME%\" INCLUDING ALL); INSERT INTO public.\"%COPY_TABLE_NAME%\" SELECT * FROM public.\"%TABLE_NAME%\"; COMMIT;"
+if %ERRORLEVEL% NEQ 0 (
+    echo Error while copying the table "%TABLE_NAME%" to "%COPY_TABLE_NAME%". Terminating the script.
+    exit /b 1
+)
+
 
 REM Final success message
 echo Pipeline completed successfully!
